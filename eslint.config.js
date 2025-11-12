eb6c2b5e13bca2cb94bafb8b4d2065406a77398d
cesarve-dev <cesar.radiate@gmail.com>
2025-11-11 16:59:35 -0800

    add small changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,7 +14,7 @@ export default defineConfig([
         version: 'detect',
       },
     },
-    plugings : {
+    plugings: {
       react,
       // react-hooks: reactHooks,
     },
@@ -40,4 +40,4 @@ export default defineConfig([
       ...react.configs['jsx-runtime'].rules,
     },
   },
-])
+]);
