fe993f9679d4e0642823ca858b33556876ad44af
cesarve-dev <cesar.radiate@gmail.com>
2025-12-01 00:38:21 -0800

    create component: TodoForm and TodoList. Add it to app.jsx

---
+++ src/TodoForm.jsx
@@ -0,0 +1,11 @@
+function TodoForm() {
+  return (
+    <form>
+      <label htmlFor="todoTile">Todo</label>
+      <input type="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
