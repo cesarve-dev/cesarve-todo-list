fb440f73ef62f9ba6394d1477d9ea4ddd836e8be
cesarve-dev <cesar.radiate@gmail.com>
2025-12-03 16:29:53 -0800

    clean up code.

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,13 +1,3 @@
-// function TodoForm() {
-//   return (
-//     <form>
-//       <label htmlFor="todoTile">Todo</label>
-//       <input type="text" />
-//       <button>Add Todo</button>
-//     </form>
-//   );
-// }
-
 const TodoForm = () => {
   return (
     <form>
