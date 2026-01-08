419e4737e1ac2beab18f92ca22448bece38e172e
cesarve-dev <cesar.radiate@gmail.com>
2026-01-08 01:00:16 -0800

    add ternary statement to display text if todolist is empty.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,9 +3,11 @@ import TodoListItem from './TodoListItem.jsx';
 function TodoList({ todoList }) {
   return (
     <ul>
-      {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
-      ))}
+      {todoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+      ) : (
+        todoList.map((todo) => <TodoListItem key={todo.id} todo={todo} />)
+      )}
     </ul>
   );
 }
