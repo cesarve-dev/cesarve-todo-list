1457046670dd1d123d0006322c82203831c49436
cesarve-dev <cesar.radiate@gmail.com>
2026-01-21 23:59:11 -0800

    Set up airtable base. Load Todos from airtable. Display error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList.jsx';
 import TodoForm from './features/TodoForm.jsx';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+      const options = {
+        method: 'GET',
+        headers: { Authorization: token },
+      };
+
+      try {
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          throw new Error(resp.message);
+        }
+        const data = await resp.json();
+        const records = data.records.map((record) => {
+          const todo = {
+            title: record.fields.title,
+            id: record.id,
+            isCompleted: record.fields.isCompleted,
+          };
+          if (!todo.isCompleted) {
+            todo.isCompleted = false;
+          }
+          return todo;
+        });
+        setTodoList([...records]);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
 
   function completeTodo(id) {
     const updatedTodos = todoList.map((todo) => {
@@ -41,7 +80,17 @@ function App() {
         todoList={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
+        isLoading={isLoading}
       />
+      {errorMessage !== '' && (
+        <>
+          <hr />
+          <p>{errorMessage}</p>
+          <button type="button" onClick={() => setErrorMessage('')}>
+            dismiss
+          </button>
+        </>
+      )}
     </div>
   );
 }
